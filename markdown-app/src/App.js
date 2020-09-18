6fd5b771cb8f1fc8b772748d1747bf70c5f8c5e4
Nestuna <nicolasantunespereira@gmail.com>
2020-09-18 19:09:37 +0200

    Mise en place form

--- markdown-app/src/App.js
+++ markdown-app/src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
 import './App.css';
+import { sampleText } from './sampleText'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className='col-sm-6'>
+          <textarea 
+            className="form-control" 
+            rows="50"
+            value={ sampleText }>
+          </textarea>
+        </div>     
+        <div className="col-sm-6">
+          <div>
+            { sampleText }
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
